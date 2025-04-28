e65c319a099741057b7c64b18c4850772fbac7b5
RahulShende717 <rahulshende785@gmail.com>
2025-04-28 14:58:17 +0530

    add code for search engine

---
+++ searchEngineFe/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchBar from './assets/components/SearchBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <SearchBar/>
+    </div>
+    </>
+  )
+}
+
+export default App
